9f98dab96ab7b0a47d8c42bd66c377f20ded242a
SandaruwanB <sandarusbandara110@gmail.com>
2023-06-11 23:22:47 +0530

    setting design

--- components/user/settings/ChangeUserData.jsx
+++ components/user/settings/ChangeUserData.jsx
@@ -1,13 +1,54 @@
-import React from 'react';
-import {View, StyleSheet, FlatList, Text, ScrollView, TextInput} from 'react-native';
+import React from 'react'
+import {View, StyleSheet, FlatList, Text, ScrollView, TextInput, ToastAndroid, TouchableOpacity} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import FetherIcon from 'react-native-vector-icons/Feather'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 const ChangeUserData = () => {
+
+    const editEmail = ()=>{
+        ToastAndroid.show("Email Cannot Edit", ToastAndroid.SHORT);
+    }
+
     return (
         <ScrollView style={styles.container}>
             <View style={styles.containerFluid}>
                 <Text style={styles.topText}>Personal Details</Text>
                 <View style={styles.topBar}></View>
-                <TextInput placeholder='Your Name' style={[styles.input, {marginTop : 10,}]}/>
+                <View style={styles.inputs}>
+                    <Text style={styles.textValues}>sandaruwanbandara.dev</Text>
+                    <FontAwesome style={styles.leftIcon} name='user'/>
+                    <FetherIcon style={[styles.rightIcon, {color : 'green'}]} name='edit'/>
+                </View>
+                <View style={styles.inputs}>
+                    <Text style={styles.textValues}>sandaruwanbandara.dev@gmail.com</Text>
+                    <Icon style={styles.leftIcon} name='email'/>
+                    <FetherIcon style={[styles.rightIcon, {color : 'red'}]} name='edit' onPress={()=>editEmail()}/>
+                </View>
+                <View style={styles.inputs}>
+                    <Text style={styles.textValues}>Not Presented</Text>
+                    <FontAwesome style={styles.leftIcon} name='address-book'/>
+                    <FetherIcon style={[styles.rightIcon, {color : 'green'}]} name='edit' onPress={()=>editEmail()}/>
+                </View>
+                <Text style={[styles.topText, {marginTop : 50,}]}>Change Password</Text>
+                <View style={styles.topBar}></View>
+                <View style={styles.passField}>
+                    <TextInput placeholder='Old Password' style={styles.input}/>
+                    <FontAwesome name='lock' style={styles.passIcon}/>
+                </View>
+                <View style={styles.passField}>
+                    <TextInput placeholder='New Password' style={styles.input}/>
+                    <FontAwesome name='lock' style={styles.passIcon}/>
+                </View>
+                <View style={styles.passField}>
+                    <TextInput placeholder='Re-Enter Password' style={styles.input}/>
+                    <FontAwesome name='lock' style={styles.passIcon}/>
+                </View>
+                <View style={{flex : 1, justifyContent : 'center',width : '100%',}}>
+                    <TouchableOpacity style={styles.btn}>
+                        <Text>Change Password</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </ScrollView>
     );
@@ -31,14 +72,55 @@ const styles = StyleSheet.create({
     topText : {
         marginLeft : 10,
     },
-    input : {
-        width : '90%',
+    heading : {
+        fontSize : 15,
+        marginTop : 5,
+        marginLeft : '10%',
+    },
+    inputs : {
+        position : 'relative',
+        paddingLeft : 20,
+        marginTop : 15,
+    },
+    textValues : {
+        paddingLeft : 30,
+        fontSize : 14,
         padding : 3,
-        fontSize : 16,
-        backgroundColor : '#E4E9F2',
-        marginLeft : '5%',
-        borderWidth : 1,
-        borderColor : 'red',
+        paddingTop : 5,
+    },
+    leftIcon : {
+        fontSize : 22,
+        position : 'absolute',
+        top : 5,
+        left : 20,
+        color : '#274BDB',
+    },
+    rightIcon : {
+        fontSize : 18,
+        position : 'absolute',
+        top : 5,
+        right : 15,
+    },
+    input : {
+        width : '100%',
+        backgroundColor : '#D9E4FF',
+        padding : 3,
+        paddingLeft : 30,
+    },
+    passField :{
+        marginTop : 20,
+        width : '80%',
+        marginLeft : '10%',
+        position : 'relative',
+    },
+    passIcon : {
+        position : 'absolute',
+        top : 6,
+        left : 10,
+        fontSize : 20,
+    },
+    btn : {
+
     }
 })
 
