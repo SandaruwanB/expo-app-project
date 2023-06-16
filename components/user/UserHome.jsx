879dcfcf920729076eb2d1c72560869ea82ae15b
SandaruwanB <sandarusbandara110@gmail.com>
2023-06-16 09:48:45 +0530

    home development

--- components/user/UserHome.jsx
+++ components/user/UserHome.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import {View, StyleSheet, Text, TouchableOpacity, ScrollView, TextInput} from 'react-native';
 import * as SecureStorage from 'expo-secure-store';
 import { useNavigation } from '@react-navigation/native';
+import { Dropdown } from 'react-native-element-dropdown';
+import IonIcons from 'react-native-vector-icons/Ionicons';
 
 const UserHome = () => {
     const [ user, setUser ] = useState("");
     const navigate = useNavigation();
+    const [category, setCategory] = useState("");
 
     const getToken = async () =>{
         try {
@@ -16,6 +19,12 @@ const UserHome = () => {
         }
     }
     getToken();
+
+    const data = [
+        { label: "PUCIT", value: "pucit" },
+        { label: "UCP", value: "ucp" },
+        { label: "UET", value: "uet" },
+    ];
     
     /*const signOut = async ()=>{
         try {
@@ -32,11 +41,34 @@ const UserHome = () => {
             <ScrollView>
                 <View style={styles.quickPost}>
                     <Text style={styles.quickText}>Quick Post</Text>
-
-                    <TextInput placeholder='Your Post' style={styles.quickPostInput} multiline={true} numberOfLines={4}/>
+                    <Dropdown 
+                        style={styles.dropdown}
+                        placeholder='What is the Category?'
+                        placeholderStyle={styles.placeholderStyle}
+                        selectedTextStyle={styles.selectedTextStyle}
+                        inputSearchStyle={styles.inputSearchStyle}
+                        iconStyle={styles.iconStyle}
+                        data={data}
+                        search
+                        onChange={ item => {
+                            setCategory(item.value);
+                        }}
+                        maxHeight={300}
+                        labelField="label"
+                        valueField="value"
+                        searchPlaceholder="Search...."
+                        value={category}
+                    />
+                    <TextInput placeholder='Your Post' style={styles.quickPostInput} multiline={true} numberOfLines={3}/>
                     <TouchableOpacity style={styles.postbtn}>
                         <Text style={styles.postBtnText}>Post</Text>
                     </TouchableOpacity>
+                </View>
+                <View style={styles.content}>
+                    <View style={styles.topLine}></View>
+                    <View style={styles.iconContent}>
+                        <IonIcons name='star' style={styles.starIcon}/>
+                    </View>
                 </View>
             </ScrollView>
         </View>
@@ -45,18 +77,22 @@ const UserHome = () => {
 
 const styles = StyleSheet.create({
     conatiner : {
-        paddingHorizontal : 15,
-        backgroundColor : '#fff',
+        backgroundColor : '#E0E0E0',
+        minHeight : '100%',
     },
     quickPost : {
         width : '100%',
-        marginTop : 10,
         padding : 10,
         position : 'relative',
+        paddingHorizontal : 20,
+        backgroundColor : '#fff',
+        marginTop : 8,
     },
     quickText : {
         marginLeft : 10,
-        paddingBottom : 10,
+        paddingBottom : 5,
+        fontWeight : 'bold',
+        fontSize : 17,
     },
     quickPostInput : {
         width : '100%',
@@ -64,9 +100,10 @@ const styles = StyleSheet.create({
         borderRadius : 5,
         textAlignVertical : 'top',
         backgroundColor : '#D9E4FF',
+        fontSize : 15,
     },
     postbtn : {
-        marginTop : 5,
+        marginTop : 10,
         backgroundColor : '#3366FF',
         width : '30%',
         padding : 4,
@@ -75,6 +112,36 @@ const styles = StyleSheet.create({
     postBtnText : {
         color : '#fff',
         textAlign : 'center',
+        fontSize : 15,
+    },
+    dropdown : {
+        backgroundColor : '#D9E4FF',
+        paddingHorizontal : 10,
+        marginBottom : 10,
+        borderRadius : 5,
+        fontSize : 15,
+    },
+    placeholderStyle : {
+        fontSize : 15,
+        color : '#2E3A59',
+    },
+    content : {
+        marginTop : 6,
+        backgroundColor : '#fff',
+        padding : 8,
+    },
+    topLine : {
+        width : '96%',
+        height : 0.7,
+        backgroundColor : '#636363',
+        marginLeft : '2%',
+    },
+    iconContent : {
+        padding : 5,
+    },
+    starIcon : {
+        fontSize : 22,
+        color : 'green',
     }
 })
 
