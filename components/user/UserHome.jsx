da9e73350e472f63ff3f1ec6c4c03062817e7bc3
SandaruwanB <sandarusbandara110@gmail.com>
2023-06-17 15:33:21 +0530

    home development

--- components/user/UserHome.jsx
+++ components/user/UserHome.jsx
@@ -65,18 +65,23 @@ const UserHome = () => {
                         <Text style={styles.postBtnText}>Post</Text>
                     </TouchableOpacity>
                 </View>
+
                 <View style={styles.content}>
                     <View style={[styles.iconContentList, { paddingHorizontal : 20, paddingBottom : 15, paddingTop : 10,}]}>
                         <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <Image source={require('../../assets/images/defaultUser.png')} style={styles.userImage}/>
-                            <View style={{marginLeft : 10,}}>
-                                <Text style={{fontSize : 15, fontWeight : 'bold',}}>Kasun Nimantha</Text>
-                                <Text style={{fontSize : 12, color : '#8F9BB3',}}>Software Engineer</Text>
-                            </View>
+                            <TouchableOpacity style={{flexDirection : 'row'}}>
+                                <Image source={require('../../assets/images/defaultUser.png')} style={styles.userImage}/>
+                                <View style={{marginLeft : 10,}}>
+                                    <Text style={{fontSize : 15, fontWeight : 'bold',}}>Kasun Nimantha</Text>
+                                    <Text style={{fontSize : 12, color : '#8F9BB3',}}>Software Engineer</Text>
+                                </View>
+                            </TouchableOpacity>
                         </View>
                         <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <IonIcons name='add-sharp' style={{fontSize : 25, color : '#3366FF',}}/>
-                            <Text style={{marginLeft : 6, fontSize : 15, marginTop : 2, fontWeight : 'bold', color : '#3366FF'}}>Follow</Text>
+                            <TouchableOpacity style={{flexDirection : 'row',}}>
+                                <IonIcons name='add-sharp' style={{fontSize : 25, color : '#3366FF',}}/>
+                                <Text style={{marginLeft : 6, fontSize : 15, marginTop : 2, fontWeight : 'bold', color : '#3366FF'}}>Follow</Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                     <View style={{padding : 20, backgroundColor : '#F2F8FF',}}>
@@ -88,7 +93,9 @@ const UserHome = () => {
                             <Text style={{paddingLeft : 4, color : '#2E3A59', fontSize : 13,}}>112 persons starred this</Text>
                         </View>
                         <View>
-                            <Text style={{color : '#2E3A59', fontSize : 13,}}>36 comments</Text>
+                            <TouchableOpacity>
+                                <Text style={{color : '#2E3A59', fontSize : 13,}}>36 comments</Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                     <View style={styles.topLine}></View>
@@ -108,97 +115,7 @@ const UserHome = () => {
                             <View></View>
                         </View>
                     </View>
-                </View>
-
-                <View style={styles.content}>
-                    <View style={[styles.iconContentList, { paddingHorizontal : 20, paddingBottom : 15, paddingTop : 10,}]}>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <Image source={require('../../assets/images/defaultUser.png')} style={styles.userImage}/>
-                            <View style={{marginLeft : 10,}}>
-                                <Text style={{fontSize : 15, fontWeight : 'bold',}}>Kasun Nimantha</Text>
-                                <Text style={{fontSize : 12, color : '#8F9BB3',}}>Software Engineer</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <IonIcons name='add-sharp' style={{fontSize : 25, color : '#3366FF',}}/>
-                            <Text style={{marginLeft : 6, fontSize : 15, marginTop : 2, fontWeight : 'bold', color : '#3366FF'}}>Follow</Text>
-                        </View>
-                    </View>
-                    <View style={{padding : 20, backgroundColor : '#F2F8FF',}}>
-                        <Text style={{fontSize : 18, textAlign : 'justify',}}>test test test test test test test test test test test test test test test test test test test test test test ?</Text>
-                    </View>
-                    <View style={[styles.iconContentList, {paddingHorizontal : 25, paddingBottom : 4, marginTop : 15,}]}>
-                        <View style={{flexDirection : 'row'}}>
-                            <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D',}]}/>
-                            <Text style={{paddingLeft : 4, color : '#2E3A59', fontSize : 13,}}>112 persons starred this</Text>
-                        </View>
-                        <View>
-                            <Text style={{color : '#2E3A59', fontSize : 13,}}>36 comments</Text>
-                        </View>
-                    </View>
-                    <View style={styles.topLine}></View>
-                    <View style={styles.iconContent}>
-                        <View style={styles.iconContentList}>
-                            <View></View>
-                            <View>
-                                <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D', display : 'none',}]}/>
-                                <IonIcons name='star-outline'  style={[styles.starIcon, {textAlign : 'center', }]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12, display : 'none',}}>Starred</Text>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Star</Text>
-                            </View>
-                            <View>
-                                <FaFa name='comment-dots' style={[styles.starIcon, {textAlign : 'center'}]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Comment</Text>
-                            </View>
-                            <View></View>
-                        </View>
-                    </View>
-                </View>
-
-                <View style={styles.content}>
-                    <View style={[styles.iconContentList, { paddingHorizontal : 20, paddingBottom : 15, paddingTop : 10,}]}>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <Image source={require('../../assets/images/defaultUser.png')} style={styles.userImage}/>
-                            <View style={{marginLeft : 10,}}>
-                                <Text style={{fontSize : 15, fontWeight : 'bold',}}>Kasun Nimantha</Text>
-                                <Text style={{fontSize : 12, color : '#8F9BB3',}}>Software Engineer</Text>
-                            </View>
-                        </View>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <IonIcons name='add-sharp' style={{fontSize : 25, color : '#3366FF',}}/>
-                            <Text style={{marginLeft : 6, fontSize : 15, marginTop : 2, fontWeight : 'bold', color : '#3366FF'}}>Follow</Text>
-                        </View>
-                    </View>
-                    <View style={{padding : 20, backgroundColor : '#F2F8FF',}}>
-                        <Text style={{fontSize : 18, textAlign : 'center',}}>test test test test test test test test test test test test test test test test test test test test test test ?</Text>
-                    </View>
-                    <View style={[styles.iconContentList, {paddingHorizontal : 25, paddingBottom : 4, marginTop : 15,}]}>
-                        <View style={{flexDirection : 'row'}}>
-                            <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D',}]}/>
-                            <Text style={{paddingLeft : 4, color : '#2E3A59', fontSize : 13,}}>112 persons starred this</Text>
-                        </View>
-                        <View>
-                            <Text style={{color : '#2E3A59', fontSize : 13,}}>36 comments</Text>
-                        </View>
-                    </View>
-                    <View style={styles.topLine}></View>
-                    <View style={styles.iconContent}>
-                        <View style={styles.iconContentList}>
-                            <View></View>
-                            <View>
-                                <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D', display : 'none',}]}/>
-                                <IonIcons name='star-outline'  style={[styles.starIcon, {textAlign : 'center', }]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12, display : 'none',}}>Starred</Text>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Star</Text>
-                            </View>
-                            <View>
-                                <FaFa name='comment-dots' style={[styles.starIcon, {textAlign : 'center'}]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Comment</Text>
-                            </View>
-                            <View></View>
-                        </View>
-                    </View>
-                </View>
+                </View>                
             </ScrollView>
         </View>
     );
