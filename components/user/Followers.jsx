430ff0e27a57183ced80e44c4b0c348183938abf
SandaruwanB <sandarusbandara110@gmail.com>
2023-06-20 22:22:24 +0530

    DEV followers

--- components/user/Followers.jsx
+++ components/user/Followers.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, ScrollView, Text} from 'react-native';
 import config from '../../apiConfig';
 import * as SecureStorage from 'expo-secure-store';
 
@@ -22,18 +22,47 @@ const Followers = () => {
         axios.post(`${config.uri}/getFollowers`, {
             email : user,
         }).then(res=>{
-            console.log(res.data);
+            if(res.data.result !== "notFound"){
+                setFollowers(res.data.followers);
+            }
         })
     })
 
 
     return (
-        <View>
-            
+        <View style={styles.container}>
+            <ScrollView>
+                <View style={styles.content}>
+                    <Text>60 Followers</Text>
+                </View>
+                <View style={styles.line}></View>
+                <View style={styles.item}>
+                    <View style={{}}>
+
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor : '#fff',
+        minHeight : '100%',
+    },
+    content : {
+        padding : 10,
+        paddingHorizontal : 30,
+    },
+    line : {
+        width : '100%',
+        height : 0.7,
+        backgroundColor : '#2E3A59',
+    },
+    item : {
+        width : '100%',
+    }
+})
 
 export default Followers;
