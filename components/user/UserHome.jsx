5bf1364748e6758e600e13b56a8b0130beedc26c
SandaruwanB <sandarusbandara110@gmail.com>
2023-07-17 12:32:11 +0530

    DEV quick post

--- components/user/UserHome.jsx
+++ components/user/UserHome.jsx
@@ -7,11 +7,13 @@ import IonIcons from 'react-native-vector-icons/Ionicons';
 import FaFa from 'react-native-vector-icons/FontAwesome5';
 import MatIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MatirialIcon from 'react-native-vector-icons/MaterialIcons';
+import axios from 'axios';
+import config from '../../apiConfig'
 
 const UserHome = () => {
     const [ user, setUser ] = useState("");
     const navigate = useNavigation();
-    const [category, setCategory] = useState("Choose category?");
+    const [category, setCategory] = useState("Chose category?");
     const [bottomTabOpen, setBottomTabOpen] = useState(false);
     const [categoryOpen, setCategoryOpen] = useState(false);
     const [postContent, setPostContent] = useState("");
@@ -29,8 +31,28 @@ const UserHome = () => {
     getToken();
 
     const quickPost = async ()=>{
-        if(category === "Choose category?"){
-            
+        if(category === "Choose category?" && postContent === ""){
+            setPostErrCategory(true);
+            setPostErrText(true);
+        }
+        else if(category === "Choose category?"){
+            setPostErrCategory(true);
+            setPostErrText(true);
+        }
+        else if(postContent === ""){
+            setPostErrCategory(false);
+            setPostErrText(true);
+        }
+        else{
+            setPostErrCategory(false);
+            setPostErrText(false);
+            await axios.post(`${config.uri}/quickPost`, {
+                user : user,
+                category : category,
+                postContent : postContent,
+            }).then(res=>{
+                console.log(res.data);
+            })
         }
     }
     
@@ -48,16 +70,23 @@ const UserHome = () => {
         <View style={styles.conatiner}>
             <ScrollView style={{height : '100%', marginBottom : 100,}}>
                 <View style={[styles.quickPost, ]}>
-                    <Text style={[styles.quickText, {paddingBottom : 10,}]}>Quick Post</Text>
-                    <View style={styles.categorySelection}>
-                        <TouchableWithoutFeedback style={{width : '100%', height : '100%'}} onPress={()=>setCategoryOpen(true)}>
+                    <Text style={[styles.quickText]}>Quick Post</Text>
+                    <View style={[styles.categorySelection, postErrCategory ? styles.errorInput : ""]}>
+                        <TouchableWithoutFeedback style={{width : '100%', height : '100%',}} onPress={()=>setCategoryOpen(true)}>
                             <Text style={{paddingLeft : 3, color : '#2E3A59'}}>{category}</Text>
-                        </TouchableWithoutFeedback>
-                    </View> 
-                    <TextInput placeholder='Your Post' style={styles.quickPostInput} onChangeText={text=>setPostContent(text)} multiline={true} numberOfLines={3}/>
-                    <TouchableOpacity style={styles.postbtn} onPress={quickPost}>
+                        </TouchableWithoutFeedback>                         
+                    </View>
+                    {
+                        postErrCategory ? <Text style={{width : '100%', textAlign : 'center', color : 'red', paddingBottom : 10,}}>This Field is Required</Text> : ""
+                    }
+                    <TextInput placeholder='Your Post' style={[styles.quickPostInput, postErrText ? styles.errorInput : ""]} onChangeText={text=>setPostContent(text)} multiline={true} numberOfLines={3}/>
+                    {
+                        postErrText ? <Text style={{width : '100%', textAlign : 'center', color : 'red', paddingTop : 10}}>This Field is Required</Text> : ""                        
+                    }
+                    <TouchableOpacity style={styles.postbtn} onPress={()=>quickPost()}>
                         <Text style={styles.postBtnText}>Post</Text>
                     </TouchableOpacity>
+                    
                 </View>
 
                 <View style={styles.content}>
@@ -108,107 +137,7 @@ const UserHome = () => {
                             <View></View>
                         </View>
                     </View>
-                </View>  
-
-                                <View style={styles.content}>
-                    <View style={[styles.iconContentList, { paddingHorizontal : 20, paddingBottom : 15, paddingTop : 10,}]}>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <TouchableOpacity style={{flexDirection : 'row'}}>
-                                <Image source={require('../../assets/images/defaultUser.png')} style={styles.userImage}/>
-                                <View style={{marginLeft : 10,}}>
-                                    <Text style={{fontSize : 15, fontWeight : 'bold', color : '#222B45',}}>Kasun Nimantha</Text>
-                                    <Text style={{fontSize : 12, color : '#8F9BB3',}}>Software Engineer</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <TouchableOpacity style={{flexDirection : 'row',}} onPress={()=>setBottomTabOpen(true)}>
-                                <MatIcons name='dots-vertical' style={styles.bottomSheetOpenIcon}/>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={{padding : 20, backgroundColor : '#F2F8FF',}}>
-                        <Text style={{fontSize : 18, textAlign : 'justify',}}>test test test test test test test test test test test test test test test test test test test test test test ?</Text>
-                    </View>
-                    <View style={[styles.iconContentList, {paddingHorizontal : 25, paddingBottom : 4, marginTop : 15,}]}>
-                        <View style={{flexDirection : 'row'}}>
-                            <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D',}]}/>
-                            <Text style={{paddingLeft : 4, color : '#2E3A59', fontSize : 13,}}>112 persons starred this</Text>
-                        </View>
-                        <View>
-                            <TouchableOpacity>
-                                <Text style={{color : '#2E3A59', fontSize : 13,}}>36 comments</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={styles.topLine}></View>
-                    <View style={styles.iconContent}>
-                        <View style={styles.iconContentList}>
-                            <View></View>
-                            <View>
-                                <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D', display : 'none',}]}/>
-                                <IonIcons name='star-outline'  style={[styles.starIcon, {textAlign : 'center', }]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12, display : 'none',}}>Starred</Text>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Star</Text>
-                            </View>
-                            <View>
-                                <FaFa name='comment-dots' style={[styles.starIcon, {textAlign : 'center'}]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Comment</Text>
-                            </View>
-                            <View></View>
-                        </View>
-                    </View>
-                </View> 
-
-                                <View style={styles.content}>
-                    <View style={[styles.iconContentList, { paddingHorizontal : 20, paddingBottom : 15, paddingTop : 10,}]}>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <TouchableOpacity style={{flexDirection : 'row'}}>
-                                <Image source={require('../../assets/images/defaultUser.png')} style={styles.userImage}/>
-                                <View style={{marginLeft : 10,}}>
-                                    <Text style={{fontSize : 15, fontWeight : 'bold', color : '#222B45',}}>Kasun Nimantha</Text>
-                                    <Text style={{fontSize : 12, color : '#8F9BB3',}}>Software Engineer</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{flexDirection : 'row', position : 'relative',}}>
-                            <TouchableOpacity style={{flexDirection : 'row',}} onPress={()=>setBottomTabOpen(true)}>
-                                <MatIcons name='dots-vertical' style={styles.bottomSheetOpenIcon}/>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={{padding : 20, backgroundColor : '#F2F8FF',}}>
-                        <Text style={{fontSize : 18, textAlign : 'justify',}}>test test test test test test test test test test test test test test test test test test test test test test ?</Text>
-                    </View>
-                    <View style={[styles.iconContentList, {paddingHorizontal : 25, paddingBottom : 4, marginTop : 15,}]}>
-                        <View style={{flexDirection : 'row'}}>
-                            <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D',}]}/>
-                            <Text style={{paddingLeft : 4, color : '#2E3A59', fontSize : 13,}}>112 persons starred this</Text>
-                        </View>
-                        <View>
-                            <TouchableOpacity>
-                                <Text style={{color : '#2E3A59', fontSize : 13,}}>36 comments</Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                    <View style={styles.topLine}></View>
-                    <View style={styles.iconContent}>
-                        <View style={styles.iconContentList}>
-                            <View></View>
-                            <View>
-                                <IonIcons name='star' style={[styles.starIcon, { color : '#FFC94D', display : 'none',}]}/>
-                                <IonIcons name='star-outline'  style={[styles.starIcon, {textAlign : 'center', }]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12, display : 'none',}}>Starred</Text>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Star</Text>
-                            </View>
-                            <View>
-                                <FaFa name='comment-dots' style={[styles.starIcon, {textAlign : 'center'}]}/>
-                                <Text style={{textAlign : 'center', color : '#2E3A59', fontSize : 12,}}>Comment</Text>
-                            </View>
-                            <View></View>
-                        </View>
-                    </View>
-                </View>               
+                </View>                
             </ScrollView>
             <BottomSheet
                 visible={bottomTabOpen}
@@ -373,6 +302,10 @@ const styles = StyleSheet.create({
         backgroundColor : '#D9E4FF',
         marginBottom : 10,
         borderRadius : 5,
+    },
+    errorInput : {
+        borderWidth : 1,
+        borderColor : 'red',
     }
 })
 
