2f52b1d966a9b95213317feae01b0ca63c6c5a67
SandaruwanB <sandarusbandara110@gmail.com>
2023-08-28 18:11:51 +0530

    DEV followers

--- components/user/followings.jsx
+++ components/user/followings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {View, StyleSheet, Image, ScrollView, TouchableOpacity} from 'react-native'
 import {Text, List, Divider} from 'react-native-paper'
 import {BottomSheet} from 'react-native-btr'
@@ -6,31 +6,62 @@ import MatIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import FaFa from 'react-native-vector-icons/FontAwesome5'
 import FaFa1 from 'react-native-vector-icons/FontAwesome'
 import IonIcons from 'react-native-vector-icons/Ionicons'
+import config from '../../apiConfig'
+import axios from 'axios'
+import * as SecureStorage from 'expo-secure-store'
 
 
 const Followings = () => {
     const [bottomSheetOpen, setBottomSheetOpen] = useState(false);
+    const [followers, setFollowers] = useState([]);
+    const [token,setToken] = useState("");
 
+    const getToken = async () =>{
+        try {
+            const result = await SecureStorage.getItemAsync('auth');
+            setToken(result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getToken();
+
+    useEffect(()=>{
+        axios.post(`${config.uri}/getFollowers`, {email : token}).then(res=>{
+            setFollowers(res.data.followers);
+            console.log(res.data);
+        })
+    })
 
     return (
         <View>
             <ScrollView>
-                <List.Section style={{backgroundColor: '#fff', paddingBottom: 10}}>
-                    <List.Subheader style={{backgroundColor: '#fff', fontSize : 16, fontWeight : 'bold', color : '#3366FF'}}>Followers</List.Subheader>
-                    <View style={{paddingHorizontal: 20}}>
-                        <Divider/>
-                    </View>
-                    <List.Item
-                        style={{backgroundColor: '#fff',paddingHorizontal : 20}}
-                        title={"Molaweson appochchi"}
-                        description={"@molaweson thama ithin"}
-                        left={()=><Image source={require('../../assets/images/defaultUser.png')} style={{width : 45, height : 45, borderRadius : 40}}/>}
-                        right={()=><MatIcons name='dots-vertical' size={30} style={{marginTop: 10}} onPress={()=>setBottomSheetOpen(true)}/>}
-                    />
-                    <View style={{paddingHorizontal: 20}}>
-                        <Divider/>
-                    </View>
-                </List.Section>
+                {
+                    followers ?
+                        followers.map((value)=>{
+                            return (
+                                <List.Section style={{backgroundColor: '#fff', paddingBottom: 10}}>
+                                    <List.Subheader style={{backgroundColor: '#fff', fontSize : 16, fontWeight : 'bold', color : '#3366FF'}}>Followers</List.Subheader>
+                                    <View style={{paddingHorizontal: 20}}>
+                                        <Divider/>
+                                    </View>
+                                    <List.Item
+                                        style={{backgroundColor: '#fff',paddingHorizontal : 20}}
+                                        title={"Molaweson appochchi"}
+                                        description={"@molaweson thama ithin"}
+                                        left={()=><Image source={require('../../assets/images/defaultUser.png')} style={{width : 45, height : 45, borderRadius : 40}}/>}
+                                        right={()=><MatIcons name='dots-vertical' size={30} style={{marginTop: 10}} onPress={()=>setBottomSheetOpen(true)}/>}
+                                    />
+                                    <View style={{paddingHorizontal: 20}}>
+                                        <Divider/>
+                                    </View>
+                                </List.Section>
+                            );
+                        })
+                    :
+                     ""
+                }
+
             </ScrollView>
             <BottomSheet
                 visible={bottomSheetOpen}
